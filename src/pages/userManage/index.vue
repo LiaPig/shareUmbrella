43172473ca2b156ab8e3dea0f4909f20944cfd5d
Lia <418607528@qq.com>
2018-01-07 22:17:34 +0800

    ‘完成了用户管理页面和查看用户详情的字段的改动’

--- src/pages/userManage/index.vue
+++ src/pages/userManage/index.vue
@@ -4,22 +4,22 @@
     <el-row class="top_row">
       <el-row class="title1">:) 用户管理</el-row>
       <el-row class="search">
-        <!--真实姓名-->
-        <el-col class="title2">真实姓名：</el-col>
+        <!--开发者平台ID-->
+        <el-col class="title2" style="width: 120px;">开发者平台ID：</el-col>
         <el-col class="input">
           <el-input
-            placeholder="请输入真实姓名"
+            placeholder="请输入"
             prefix-icon="el-icon-search"
             v-model="searchData.realName">
           </el-input>
         </el-col>
         <!--用户名-->
-        <el-col class="title2">用户名：</el-col>
+        <el-col class="title2">微信名：</el-col>
         <el-col class="input">
           <el-input
-            placeholder="请输入用户名"
+            placeholder="请输入微信名"
             prefix-icon="el-icon-search"
-            v-model="searchData.userName">
+            v-model="searchData.nickName">
           </el-input>
         </el-col>
         <!--查询按钮-->
@@ -45,33 +45,70 @@
           </el-table-column>
           <el-table-column
             sortable
-            prop="userName"
-            label="用户名"
-            width="150">
-          </el-table-column>
-          <el-table-column
-            sortable
-            prop="realName"
-            label="真实姓名"
-            width="150">
-          </el-table-column>
-          <el-table-column
-            sortable
-            prop="phone"
-            label="联系方式"
-            width="150">
-          </el-table-column>
-          <el-table-column
-            sortable
-            prop="email"
-            label="邮箱"
+            prop="openId"
+            label="开发者平台ID"
             width="200">
           </el-table-column>
           <el-table-column
             sortable
-            prop="address"
-            label="地址"
-            width="300">
+            prop="nickName"
+            label="微信名"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            sortable
+            prop="gender"
+            label="性别"
+            width="100">
+            <template slot-scope="scope">
+              <span v-if="scope.row.gender === 1">男</span>
+              <span v-else-if="scope.row.gender === 2">女</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            sortable
+            prop="deposit"
+            label="押金(元)"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            sortable
+            prop="usageCount"
+            label="租借次数"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            label="微信头像"
+            width="150">
+            <template slot-scope="scope">
+              <el-button v-popover:popover1 size="small">点击查看</el-button>
+              <el-popover
+                ref="popover1"
+                placement="top-start"
+                width="200"
+                trigger="click">
+                <img :src="scope.row.avatarUrl" style="width: 200px;height: 200px"/>
+              </el-popover>
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            sortable
+            prop="city"
+            label="城市"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            sortable
+            prop="province"
+            label="省份"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            sortable
+            prop="country"
+            label="国家"
+            width="150">
           </el-table-column>
           <el-table-column
             label="状态"
@@ -100,44 +137,62 @@
     <!--用户详情弹窗-->
     <el-dialog title="查看用户详情" :visible.sync="showDetail">
       <el-row class="dialogDetail">
-        <!--用户名／真实姓名-->
+        <!--微信头像／微信ID-->
         <el-row style="padding-top: 30px">
-          <!--用户名-->
+          <!--微信头像-->
           <el-col :span="11">
-            <el-col :span="12" class="title">◆ 用户名：</el-col>
-            <el-col :span="12" class="content">{{detail.userName}}</el-col>
+            <el-col :span="12" class="title">◆ 微信头像：</el-col>
+            <el-col :span="12" class="content">
+              <img :src="detail.avatarUrl" style="width: 50px;height: 50px;border: 1px solid darkgrey"/>
+            </el-col>
           </el-col>
-          <!--真实姓名-->
+          <!--微信ID-->
           <el-col :span="9">
-            <el-col :span="12" class="title">◆ 真实姓名：</el-col>
-            <el-col :span="12" class="content">{{detail.realName}}</el-col>
+            <el-col :span="12" class="title">◆ 微信&nbsp;&nbsp;ID&nbsp;：</el-col>
+            <el-col :span="12" class="content">{{detail.openId}}</el-col>
           </el-col>
         </el-row>
-        <!--手机号码／邮箱-->
-        <el-row style="padding-top: 10px">
-          <!--手机号码-->
+        <!--微信名称／性别-->
+        <el-row style="padding-top: 20px">
+          <!--微信名称-->
           <el-col :span="11">
-            <el-col :span="12" class="title">◆ 手机号：</el-col>
-            <el-col :span="12" class="content">{{detail.phone}}</el-col>
+            <el-col :span="12" class="title">◆ 微信名称：</el-col>
+            <el-col :span="12" class="content">{{detail.nickName}}</el-col>
           </el-col>
-          <!--邮箱-->
+          <!--性别-->
           <el-col :span="9">
-            <el-col :span="12" class="title">◆ 邮箱地址：</el-col>
-            <el-col :span="12" class="content">{{detail.email}}</el-col>
+            <el-col :span="12" class="title">◆ 性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别&nbsp;：</el-col>
+            <el-col :span="12" class="content">
+              <span v-if="detail.gender === 1">男</span>
+              <span v-else-if="detail.gender === 2">女</span>
+            </el-col>
           </el-col>
         </el-row>
-        <!--地址-->
-        <el-row style="padding-top: 10px;">
-          <!--地址-->
+        <!--押金金额／租借次数-->
+        <el-row style="padding-top: 20px">
+          <!--押金金额-->
+          <el-col :span="11">
+            <el-col :span="12" class="title">◆ 押金金额：</el-col>
+            <el-col :span="12" class="content">{{detail.deposit}}&nbsp;元</el-col>
+          </el-col>
+          <!--租借次数-->
+          <el-col :span="9">
+            <el-col :span="12" class="title">◆ 租借次数：</el-col>
+            <el-col :span="12" class="content">{{detail.usageCount}}&nbsp;次</el-col>
+          </el-col>
+        </el-row>
+        <!--国家地区／微信ID-->
+        <el-row style="padding-top: 20px">
+          <!--国家地区-->
           <el-col :span="22">
-            <el-col :span="6" class="title">◆ 地址：</el-col>
-            <el-col :span="16" class="content">{{detail.address}}</el-col>
+            <el-col :span="6" class="title">◆ 国家地区：</el-col>
+            <el-col :span="16" class="content">{{detail.country+ " " + detail.province+ " " +detail.city}}</el-col>
           </el-col>
         </el-row>
         <!--状态-->
-        <el-row style="padding-top: 10px;padding-bottom: 30px;">
+        <el-row style="padding-top: 20px;padding-bottom: 30px;">
           <el-col :span="22">
-            <el-col :span="6" class="title">◆ 状态：</el-col>
+            <el-col :span="6" class="title">◆ 状&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;态&nbsp;：</el-col>
             <el-col :span="16" class="content">
               <span v-if="detail.status === '1'">正常</span>
               <span v-else>禁用</span>
@@ -158,87 +213,123 @@
       return {
         // 搜索框的数据
         searchData: {
-          realName: "",
-          userName: ""
+          openId: "",
+          nickName: ""
         },
         // 表格数据
         tableData: [
           {
             id: "1",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           },
           {
             id: "2",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           },
           {
             id: "3",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           },
           {
             id: "4",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           }, {
             id: "5",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           }, {
             id: "6",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           },
           {
             id: "7",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           },
           {
             id: "8",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           }, {
             id: "9",
-            userName: "huangliya",
-            realName: '黄猪崽',
-            phone: "13246825048",
-            email: "418607528@qq.com",
-            address: "广东药科大学生活区1栋362",
+            openId: "8008208820",
+            nickName: "Lia黄丽雅",
+            avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/d9ZyJzoqENWX6bmqJee1ZwONZYY01wib2KAIibT1KQIZhIBdnRGtg4bicMbb7Eia809lSIUNOyWHvr9iaPjTnrjWx0Q/0",
+            city: "Guangzhou",
+            province: "Guangdong",
+            country: "China",
+            gender: 2,
+            deposit: 200,
+            usageCount: 1,
             status: "1"
           },
         ],
@@ -279,18 +370,9 @@
       },
       // 查看详情
       handleDetail(data) {
+        this.detail = data;
+        // 打开弹窗
         this.showDetail = true;
-
-        this.detail = {
-          id: "1",
-          userName: "huangliya",
-          realName: '黄猪崽' + data.id,
-          phone: "13246825048",
-          email: "418607528@qq.com",
-          address: "广东药科大学生活区1栋362",
-          status: "1"
-        }
-//        alert(data.realName)
       }
 
     }
@@ -298,8 +380,6 @@
 </script>
 
 <style>
-
-
   .title2 {
     width: 70px;
     height: 60px;
