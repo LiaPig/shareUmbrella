34bc0fd3fe67e161ee9a1b3f0952633bda790aee
Lia <418607528@qq.com>
2018-02-18 22:35:48 +0800

    优化了编辑菜单

--- src/api/menu.js
+++ src/api/menu.js
@@ -1,10 +1,10 @@
-// export getMenus =
-
 // 获取所有菜单
 export const getMenus = `api/menus`;
 // 新增一条菜单
 export const addMenu = `api/menus`;
 // 删除一条菜单
 export const deleteMenu = `api/menus/`;
+// 根据id获取某菜单信息
+export const getMenuById = `api/menus/`;
 // 编辑更新菜单信息
 export const updateMenu = `api/menus/update`;
--- src/pages/menuManage/index.vue
+++ src/pages/menuManage/index.vue
@@ -170,7 +170,7 @@
 </template>
 
 <script>
-  import {addMenu, getMenus, deleteMenu, updateMenu} from 'Api/menu'
+  import {addMenu, getMenus, deleteMenu, getMenuById, updateMenu} from 'Api/menu'
 
   export default {
     data() {
@@ -248,9 +248,16 @@
       handleEdit(data) {
         this.formType = 2;
         this.formTitle = "编辑菜单";
-        this.form = data;
-        // 打开弹窗
-        this.showDialog = true;
+        this.$http.get(getMenuById + data.id)
+          .then(res => {
+            this.form = res.data.data;
+            // 打开弹窗
+            this.showDialog = true;
+          })
+          .catch(err => {
+            console.error(err);
+          })
+
       },
       // 点击弹窗里的确认按钮(formType,2为编辑)
       formSubmit() {
